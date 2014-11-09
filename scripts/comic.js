932e2449f7a602eca02eb03ee1036d0b9c1b5241
Ori <opleban@gmail.com>
2014-11-08 21:32:41 -0800

    fixes pageNumber loop

--- scripts/comic.js
+++ scripts/comic.js
@@ -8,14 +8,16 @@ var ComicViewer = (function(){
       var thumbnails = document.getElementsByClassName('thumbnail');
       for (var i=0; i<thumbnails.length; i++){
         var thumbnail = thumbnails[i];
-        thumbnail.addEventListener('click', function(e){
-          e.preventDefault();
-          var pageNumber = this.id.slice(-1);
-          fullPageImage.setAttribute('src', IMG_BASE_PATH + pageNumber + '.jpg');
-          currentImage.className = 'thumbnail';
-          this.className += ' selected';
-          currentImage = this;
-        });
+        (function(i){
+          thumbnail.addEventListener('click', function(e){
+            e.preventDefault();
+            var pageNumber = i+1;
+            fullPageImage.setAttribute('src', IMG_BASE_PATH + pageNumber + '.jpg');
+            currentImage.className = 'thumbnail';
+            this.className += ' selected';
+            currentImage = this;
+          });
+        }(i));
       }
     }
   };
