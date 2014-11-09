60d49d8f80e19543e298b85ca338269cf584c641
Ori <opleban@gmail.com>
2014-11-08 20:53:53 -0800

    optimizes script to target li instead of img

--- scripts/comic.js
+++ scripts/comic.js
@@ -2,9 +2,10 @@ var ComicViewer = (function(){
   var IMG_BASE_PATH = 'assets/images/EscapeComics/ESCAPE_color_';
   return {
     init:function(){
-      var currentImage = document.getElementById('page-1').parentElement;
-      var fullPageImage = document.getElementById("full-page");
-      var thumbnails = document.getElementsByClassName('thumbnail-image');
+      var currentImage = document.getElementById('thumbnail-1');
+      currentImage.className += ' selected';
+      var fullPageImage = document.getElementById('full-page');
+      var thumbnails = document.getElementsByClassName('thumbnail');
       for (var i=0; i<thumbnails.length; i++){
         var thumbnail = thumbnails[i];
         thumbnail.addEventListener('click', function(e){
@@ -12,14 +13,14 @@ var ComicViewer = (function(){
           var pageNumber = this.id.slice(-1);
           fullPageImage.setAttribute('src', IMG_BASE_PATH + pageNumber + '.jpg');
           currentImage.className = 'thumbnail';
-          this.parentElement.className += ' selected';
-          currentImage = this.parentElement;
+          this.className += ' selected';
+          currentImage = this;
         });
       }
     }
   };
 }());
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener('DOMContentLoaded', function(){
   ComicViewer.init();
 });
